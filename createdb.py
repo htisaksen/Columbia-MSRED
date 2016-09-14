7a4843453e153b017431b1f7d5c0be25d620f470
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2016-09-14 12:28:14 -0400

    added virtual env,database(postgres)

---
+++ createdb.py
@@ -0,0 +1,28 @@
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'postgresql://username:password@hostname/database
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True) name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+if __name__=="__main__":
+    
