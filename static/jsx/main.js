a7ce9c4e35a911e473f5c92f4af0d521f87c4723
Freddie71010 <freddie71010@gmail.com>
2016-09-15 17:41:08 -0400

    dashboard.html now adds and deletes rows for the Rental Rate Assumptions table

--- static/jsx/main.js
+++ static/jsx/main.js
@@ -3,14 +3,23 @@ $(document).ready(function(){
 
 	$('#calc_submit').on('submit', function(event){
 		console.log('submitted clicked=============')
-		('#Purchase_Price').attr(100);
-		('#Purchase_Price').val(100);
 	});
 
-	var counter = 1;
-	var testprint = "<input type='text' name='proj_rents_"+counter+"' id='proj_rents_"+counter+"'>";
-	console.log(testprint);
-			
+
+
+	// $("#proj_rents_1").on('input', appOnInput);
+	// $("#total_units_1").on('input', appOnInput);
+	// var appOnInput = function(event){
+	// 	var f = parseInt($("#in-freddie").val()) || 0;
+	// }
+	
+	//calculations for Rental Rate Assumptions table
+	var rental_rate_calc = function() {
+		
+	}
+	
+
+	var counter = 2;
 	$("#rental_rate_form").on('click',(function(event) {
 		console.log('clicked add_rental_rates button');
 		event.preventDefault();
@@ -18,18 +27,44 @@ $(document).ready(function(){
 			method: 'POST',
 			url: '/dashboard',
 			success: function(response) {
+				console.log(counter);
 				$('#Rental_Rate_Assumptions').find('tbody')
-					.append($('<tr>')
-						.append($('<td>')
-							.html("<input type='text' name='proj_rents_"+counter+"' id='proj_rents_"+counter+"' placeholder='Project Rents #"+counter+"'>")
-							)
-						.append($('<td>')
-							.text('Test text here we are!')
-							)
-					)
-				counter += 1;
+					.append($("<tr id = 'rent_row_"+counter+"'>")
+						.append($("<td>").html("<input type='text' name='proj_rents_"+counter+"' id='proj_rents_"+counter+"' placeholder='Project Rents #"+counter+"'>"))
+						.append($("<td>").html("<input type='text' name='total_units_"+counter+"' id='total_units_"+counter+"' placeholder='Total Units #"+counter+"'>"))
+						.append($("<td id = 'total_sf_"+counter+"'>"))
+						.append($("<td>").html("<input type='text' name='avg_sf_per_unit_"+counter+"' id='avg_sf_per_unit_"+counter+"' placeholder='Avg SF/Unit #"+counter+"'>"))
+						.append($("<td id = 'rent_per_sf_"+counter+"'>"))
+						.append($("<td id = 'rent_per_unit_"+counter+"'>"))
+						.append($("<td>").html("<a>[X]</a>"))
+						)
+				counter = counter + 1;
+				
+				// function to delete all additionally added rows from the Rental Rate Assumptions table
+				$('tr[id^="rent_row_"] a').on('click', function(event) {
+					console.log("clicked on X to delete row");
+					// you could ge the id number from the tr 
+					var id = $(this).closest('tr').attr('id').replace("rent_row_","");
+					//then you could remove anything the that ends with _id 
+					$('[id$="_'+id+'"]').remove();
+				});
+
+				$(this).on('input', rental_rate_calculations);
+
+
 			} // end success
 		}) //end ajax
 	}));
+
+	// function to delete initial row from the Rental Rate Assumptions table
+	$('tr[id^="rent_row_"] a').on('click', function(event) {
+		console.log("clicked on X to delete row");
+		// you could ge the id number from the tr 
+		var id = $(this).closest('tr').attr('id').replace("rent_row_","");
+		//then you could remove anything the that ends with _id 
+		$('[id$="_'+id+'"]').remove();
+	});
+
+
 
 }) //end of doc
