3d8f6512fad3e769b9ec37b95d8f430bf87d67a9
Freddie71010 <freddie71010@gmail.com>
2016-09-19 17:10:05 -0400

    dashboard.html - RR Assumption table - row calculations work! WIP sum calcuations

--- static/jsx/main.js
+++ static/jsx/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-//Javascript rental rate first insert row
+//Javascript rental rate first insert row -------------------------------------------
 	$('#Rental_Rate_Assumptions').find('tbody')
 		.append($("<tr class = 'rent_row'>")
 		.append($("<td>").html("<input type='text' name='proj_rents' class='proj_rents' placeholder='Project Rents'></td>"))
@@ -9,46 +9,79 @@ $(document).ready(function(){
 		.append($("<td class = 'total_sf'></td>").text("0"))
 		.append($("<td>").html("<input type='text' name='avg_sf_per_unit' class='avg_sf_per_unit' placeholder='Avg SF/Unit'></td>"))
 		.append($("<td class = 'rent_per_sf'></td>").text("$0.00"))
-		.append($("<td>").html("<input type='text' name='rent_per_unit' class='rent_per_unit' placeholder='Cost Per Unit'></td>"))
+		.append($("<td>").html("<input type='text' name='rent_per_unit' class='rent_per_unit' placeholder='Rent Per Unit'></td>"))
 		)
-// Global objects: Inputs (possibly calculated values)
+
+// Global objects: Inputs (possibly calculated values) -------------------------------------------
 	var g = {}
 
-
 	var rentalRows = []
+
+
+
+// Calculates ALL Rental Rate Assumptions calculations -------------------------------------------
+	// Calculates Rental Rate Assumptions row calculations
 	var applyOnInput = function(event){
 		var rrCounter = $('#Rental_Rate_Assumptions tbody tr').length;
-		console.log("rrcounter: ",rrCounter);
-		console.log($('this'));
-		$('this > tr.rent_row > td').html("I have replaced the entire row of text");
-		console.log("you have already clicked on this");
+		total_units = parseInt($('.total_units', this).val());
+		avg_sf_per_unit = parseInt($(".avg_sf_per_unit", this).val());
+		rent_per_unit = parseInt($(".rent_per_unit", this).val());
+		// console.log("Total Units: ",total_units," Avg SF Unit: ",avg_sf_per_unit," Rent per unit: ",cost_per_unit);
+
+		var total_sf = total_units*avg_sf_per_unit;
+		var rent_per_sf = rent_per_unit/avg_sf_per_unit;
+
+		$('.total_sf', this).text(total_sf);
+		$('.rent_per_sf', this).text(rent_per_sf);
+		
+		// Calculates Rental Rate Assumptions column sum calculations
+		var tu = $('#Rental_Rate_Assumptions tbody .total_units');
+		var tsf = $('#Rental_Rate_Assumptions tbody .total_sf');
+		
+		sum_total_units = 0;
+		sum_total_sf = 0;
+		sum_rent_per_unit = 0;
+
+		tu.each(function(){
+			console.log(this);
+			sum_total_units = sum_total_units + parseInt($(this).val());
+		});
+
+		tsf.each(function(){
+			console.log(this);
+			sum_total_sf = sum_total_sf + parseInt($(this).text());
+		});
+		
+		sum_rent_per_unit = sum_rent_per_unit + (sum_total_units * sum_total_sf);
+		console.log("sum_rent_per_unit:",sum_rent_per_unit);
+
+		sum_avg_sf_per_unit = sum_total_sf/sum_total_units;
+		console.log("sum_avg_sf_per_unit:",sum_avg_sf_per_unit);
+		sum_rent_per_sf = sum_rent_per_unit/sum_avg_sf_per_unit;
+		console.log("sum_rent_per_sf:",sum_rent_per_sf);
+		sum_rent_per_unit = sum_rent_per_unit/sum_total_units;
+		console.log("sum_rent_per_unit:",sum_rent_per_unit);
+		
+		$('#Rental_Rate_Assumptions tfoot .total_units').text(sum_total_units);
+		$('#Rental_Rate_Assumptions tfoot .total_sf').text(sum_total_sf);
+		$('#Rental_Rate_Assumptions tfoot .avg_sf_per_unit').text(sum_avg_sf_per_unit);
+		$('#Rental_Rate_Assumptions tfoot .rent_per_sf').text(sum_rent_per_sf);
+		$('#Rental_Rate_Assumptions tfoot .rent_per_unit').text(sum_rent_per_unit);
+
+	}; //end applyOnInput function 
 
 
-		// for(var i = 1; i <= rrCounter; ++i){
-		// 	// console.log("i= ",i);
-		// 	total_units = parseInt($("#total_units_"+i).val());
-		// 	avg_sf_per_unit = parseInt($("#avg_sf_per_unit_"+i).val());
-		// 	cost_per_unit = parseInt($("#cost_per_unit_"+i).val());
-		// 	rentalRows.push({total_units:total_units,avg_sf_per_unit:avg_sf_per_unit,cost_per_unit:cost_per_unit});
-		// 	// console.log(rentalRows)
-		// };
 
-//specific example test ===============================================
-		// total_units_1 = parseInt($("#total_units_1").val());
-		// avg_sf_per_unit_1 = parseInt($("#avg_sf_per_unit_1").val());
-		// cost_per_unit_1 = parseInt($("#cost_per_unit_1").val());
-		// console.log("Total Units: ",total_units_1," Avg SF Unit: ",avg_sf_per_unit_1," cost per unit: ",cost_per_unit_1);
 
-	}; //end applyOnInput function
+	//Runs input function
+	$('.rent_row').on('input', applyOnInput);
 
-	$('tr').on('input', applyOnInput);
+//--------------------------------------------------------------------------------------
 
 
 
 
-
-
-	//Rental Rate Form Dynamic Table
+	//Rental Rate Form Dynamic Table -------------------------------------------
 	$("#rental_rate_form").on('click', function(event) {
 		console.log('clicked add_rental_rates button');
 		var rrCounter = $('#Rental_Rate_Assumptions tbody tr').length+1;
@@ -59,24 +92,24 @@ $(document).ready(function(){
 			url: '/dashboard',
 			success: function(response) {
 				$('#Rental_Rate_Assumptions').find('tbody')
-					.append($("<tr id = 'rent_row_"+rrCounter+"'>")
-					.append($("<td>").html("<input type='text' name='proj_rents_"+rrCounter+"' id='proj_rents_"+rrCounter+"' placeholder='Project Rents'></td>"))
-					.append($("<td>").html("<input type='text' name='total_units_"+rrCounter+"' id='total_units_"+rrCounter+"' placeholder='Total Units'></td>"))
-					.append($("<td id = 'total_sf_"+rrCounter+"'></td>").text("0"))
-					.append($("<td>").html("<input type='text' name='avg_sf_per_unit_"+rrCounter+"' id='avg_sf_per_unit_"+rrCounter+"' placeholder='Avg SF/Unit'></td>"))
-					.append($("<td id = 'rent_per_sf_"+rrCounter+"'></td>").text("$0.00"))
-					.append($("<td>").html("<input type='text' name='rent_per_unit_"+rrCounter+"' id='rent_per_unit_"+rrCounter+"' placeholder='Cost Per Unit'></td>"))
-					.append($("<td>").html("<a>[X]</a></td>"))
+					.append($("<tr class = 'rent_row'>")
+					.append($("<td>").html("<input type='text' name='proj_rents' class='proj_rents' placeholder='Project Rents'></td>"))
+					.append($("<td>").html("<input type='text' name='total_units' class='total_units' placeholder='Total Units'></td>"))
+					.append($("<td class = 'total_sf'></td>").text("0"))
+					.append($("<td>").html("<input type='text' name='avg_sf_per_unit' class='avg_sf_per_unit' placeholder='Avg SF/Unit'></td>"))
+					.append($("<td class = 'rent_per_sf'></td>").text("$0.00"))
+					.append($("<td>").html("<input type='text' name='rent_per_unit' class='rent_per_unit' placeholder='Rent Per Unit'></td>"))
+					.append($('<td>').html("<a>[X]</a></td>"))
 					)
 
-				// function to delete all additionally added rows from the Rental Rate Assumptions table
+				// function to delete all additionally added rows from the Rental Rate Assumptions table -------------------------------------------
 				$('tr[id^="rent_row_"] a').on('click', function(event) {
 					console.log("clicked on X to delete row");
 					$(this).parent().parent().remove();
 					rrCounter = rrCounter - 1;
 				});
-				// dynamic input calculations
-				$('tr').on('input', applyOnInput);
+				// dynamic input calculations -------------------------------------------
+				$('.rent_row').on('input', applyOnInput);
 
 
 
