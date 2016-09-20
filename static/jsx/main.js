76beaa24076dd75ff9e76ec9f1918aa8db6da09a
Freddie71010 <freddie71010@gmail.com>
2016-09-20 12:25:14 -0400

    1) dashboard.html - added Market Rental Assumptions dynamic table html, updated text in Rental Rate table from '/' to 'Per'. 2) main.js - created Market Rental Assumptions table - add row, delete row

--- static/jsx/main.js
+++ static/jsx/main.js
@@ -36,23 +36,36 @@ $(document).ready(function(){
 		// }
 
 
-//Javascript rental rate first insert row -------------------------------------------
+//Javascript Rental Rate Assumptions first insert row -------------------------------------------
 	$('#Rental_Rate_Assumptions').find('tbody')
 		.append($("<tr class = 'rent_row'>")
 		.append($("<td>").html("<input type='text' name='proj_rents' class='proj_rents' placeholder='Project Rents'></td>"))
 		.append($("<td>").html("<input type='text' name='total_units' class='total_units' placeholder='Total Units'></td>"))
 		.append($("<td class = 'total_sf'></td>").text("0"))
-		.append($("<td>").html("<input type='text' name='avg_sf_per_unit' class='avg_sf_per_unit' placeholder='Avg SF/Unit'></td>"))
+		.append($("<td>").html("<input type='text' name='avg_sf_per_unit' class='avg_sf_per_unit' placeholder='Avg SF Per Unit'></td>"))
 		.append($("<td class = 'rent_per_sf'></td>").text("$0.00"))
 		.append($("<td>").html("<input type='text' name='rent_per_unit' class='rent_per_unit' placeholder='Rent Per Unit'></td>"))
 		)
+//Javascript Market Rental Rate Assumptions first insert row -------------------------------------------
+	$('#Market_Rental_Assumptions').find('tbody')
+		.append($("<tr class = 'year_row'>")
+		.append($("<td></td>"))
+		.append($("<td></td>"))
+		.append($("<td>").html("<input type='text' name='mktRentRevenue' class='mktRentRevenue' placeholder='Revenue (%)'></td>"))
+		.append($("<td>").html("<input type='text' name='mktRentExpenses' class='mktRentExpenses' placeholder='Expenses (%)'></td>"))
+		.append($("<td>").html("<input type='text' name='mktRentVacancy' class='mktRentVacancy' placeholder='Vacancy (%)'></td>"))
+		.append($("<td>").html("<input type='text' name='mktRentConcessions' class='mktRentConcessions' placeholder='Concessions (%)'></td>"))
+		.append($("<td>").html("<input type='text' name='mktRentCreditLoss' class='mktRentCreditLoss' placeholder='Credit Loss (%)'></td>"))
+		)
 
 
-
+// ============================================================================================================
 // Start of ALL calculations for Rental Rate Assumptions table --------------------------------------------------------------------------------------
+// ============================================================================================================
+
 	var applyOnInput = function(event){
 
-	//RENTAL RATE ASSUMPTIONS
+	//RENTAL RATE ASSUMPTIONS Table
 		// Calculates row data for Rental Rate Assumptions -------------------------------------------
 		total_units = parseInt($('.total_units', this).val());
 		avg_sf_per_unit = parseInt($(".avg_sf_per_unit", this).val());
@@ -73,23 +86,26 @@ $(document).ready(function(){
 		sum_total_sf = 0;
 		SP_list_rpu = 0;
 
+		//calculates total value: Total Units
 		$tu.each(function(){
-			sum_total_units += parseInt($(this).val());
+			sum_total_units += parseInt($(this).val()); 
 		});
 
+		//calculates total value: Total SF
 		$tsf.each(function(){
 			sum_total_sf += parseInt($(this).text());
 		});
 
+		//calculates total value: Rent Per Unit
 		$rrow.each(function(){
 			num_units = $(this).find('.total_units').val();
 			rent_units = $(this).find('.rent_per_unit').val();
 			SP_list_rpu = SP_list_rpu + (num_units * rent_units);
 		})
 
-		sum_avg_sf_per_unit = sum_total_sf/sum_total_units;
-		SP_list_rpu = SP_list_rpu/sum_total_units;
-		sum_rent_per_sf = SP_list_rpu/sum_avg_sf_per_unit;
+		sum_avg_sf_per_unit = sum_total_sf/sum_total_units; //calculates total value: Avg SF Per Unit
+		SP_list_rpu = SP_list_rpu/sum_total_units; 		//calculates total value: Rent Per Unit
+		sum_rent_per_sf = SP_list_rpu/sum_avg_sf_per_unit;		//calculates total value: Rent Per SF
 
 		//appends total values to dashboard
 		$('#Rental_Rate_Assumptions tfoot .total_units').text(sum_total_units);
@@ -97,39 +113,92 @@ $(document).ready(function(){
 		$('#Rental_Rate_Assumptions tfoot .avg_sf_per_unit').text(sum_avg_sf_per_unit);
 		$('#Rental_Rate_Assumptions tfoot .rent_per_sf').text(sum_rent_per_sf);
 		$('#Rental_Rate_Assumptions tfoot .rent_per_unit').text(SP_list_rpu);
-	//END RENTAL RATE Assumptions
+	//END RENTAL RATE ASSUMPTIONS Table
 
 	}; //end applyOnInput function
 
 	//Runs input function
 	$('.rent_row').on('input', applyOnInput);
 
-//--------------------------------------------------------------------------------------
 
-
-
-
-	//Rental Rate Form Dynamic Table -------------------------------------------
+// ============================================================================================================
+//Rental Rate Form Dynamic Table
+// ============================================================================================================
+	// adds one row to table when the 'add' button is clicked
 	$("#rental_rate_form").on('click', function(event) {
 		event.preventDefault();
-				$('#Rental_Rate_Assumptions').find('tbody')
-					.append($("<tr class = 'rent_row'>")
-					.append($("<td>").html("<input type='text' name='proj_rents' class='proj_rents' placeholder='Project Rents'></td>"))
-					.append($("<td>").html("<input type='text' name='total_units' class='total_units' placeholder='Total Units'></td>"))
-					.append($("<td class = 'total_sf'></td>").text("0"))
-					.append($("<td>").html("<input type='text' name='avg_sf_per_unit' class='avg_sf_per_unit' placeholder='Avg SF/Unit'></td>"))
-					.append($("<td class = 'rent_per_sf'></td>").text("$0.00"))
-					.append($("<td>").html("<input type='text' name='rent_per_unit' class='rent_per_unit' placeholder='Rent Per Unit'></td>"))
-					.append($('<td>').html("<a>[X]</a></td>"))
-					)
+		$('#Rental_Rate_Assumptions').find('tbody')
+			.append($("<tr class = 'rent_row'>")
+			.append($("<td>").html("<input type='text' name='proj_rents' class='proj_rents' placeholder='Project Rents'></td>"))
+			.append($("<td>").html("<input type='text' name='total_units' class='total_units' placeholder='Total Units'></td>"))
+			.append($("<td class = 'total_sf'></td>").text("0"))
+			.append($("<td>").html("<input type='text' name='avg_sf_per_unit' class='avg_sf_per_unit' placeholder='Avg SF/Unit'></td>"))
+			.append($("<td class = 'rent_per_sf'></td>").text("$0.00"))
+			.append($("<td>").html("<input type='text' name='rent_per_unit' class='rent_per_unit' placeholder='Rent Per Unit'></td>"))
+			.append($('<td>').html("<a>[X]</a></td>"))
+			)
 
-				// function to delete all additionally added rows from the Rental Rate Assumptions table -------------------------------------------
-				$('tr[class^="rent_row"] a').on('click', function(event) {
-					$(this).parent().parent().remove();
-					applyOnInput();
-				});
-				//Runs input function inside  -------------------------------------------
-				$('.rent_row').on('input', applyOnInput);
+		// function to delete all additionally added rows from the Rental Rate Assumptions table 
+		$('tr[class^="rent_row"] a').on('click', function(event) {
+			$(this).parent().parent().remove();
+			applyOnInput();
+		});
+		//Runs input function inside
+		$('.rent_row').on('input', applyOnInput);
 
-		}) //end addrow
-	}); //end of doc
+	}) //end addrow function
+
+
+
+
+
+
+
+
+	// RETURNS SUMMARY
+	// END RETURN SUMMARY
+
+	// CURRENT FINANCIALS
+	// END CURRENT FINANCIALS
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============================================================================================================
+//MARKET RENTAL ASSUMPTIONS Dynamic Table
+// ============================================================================================================
+	// adds one row to table when the 'add' button is clicked
+	$("#market_rental_form").on('click', function(event) {
+		event.preventDefault();
+		$('#Market_Rental_Assumptions').find('tbody')
+			.append($("<tr class = 'year_row'>")
+			.append($("<td></td>"))
+			.append($("<td></td>"))
+			.append($("<td>").html("<input type='text' name='mktRentRevenue' class='mktRentRevenue' placeholder='Revenue (%)'></td>"))
+			.append($("<td>").html("<input type='text' name='mktRentExpenses' class='mktRentExpenses' placeholder='Expenses (%)'></td>"))
+			.append($("<td>").html("<input type='text' name='mktRentVacancy' class='mktRentVacancy' placeholder='Vacancy (%)'></td>"))
+			.append($("<td>").html("<input type='text' name='mktRentConcessions' class='mktRentConcessions' placeholder='Concessions (%)'></td>"))
+			.append($("<td>").html("<input type='text' name='mktRentCreditLoss' class='mktRentCreditLoss' placeholder='Credit Loss (%)'></td>"))
+			.append($('<td>').html("<a>[X]</a></td>"))
+			)
+
+		// function to delete all additionally added rows
+		$('tr[class^="year_row"] a').on('click', function(event) {
+			$(this).parent().parent().remove();
+			applyOnInput();
+		});
+
+	}); //end addrow function
+
+
+// ============================================================================================================
+}); //end of doc
