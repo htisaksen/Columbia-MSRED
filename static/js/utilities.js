964550bd4de206886da71021071b199a24a1a3ac
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2016-09-26 19:05:14 -0400

    test stuff

--- static/js/utilities.js
+++ static/js/utilities.js
@@ -2,55 +2,61 @@
 // Functions created for dashboard
 // ============================================================================================================
 //Returns int val of element
+myApp.utils = {};
+
+(function(){
+  
+      var myApp.utils.remSpcChr = function(value){
+		    return value.replace(/[&\/\\#,+()$~%'":*?<>{}]/g, '')
+      };
+
+      var myApp.utils.pFloat = function(value){
+    		return parseFloat(remSpcChr($(value).text()) || 0)
+    	};
+
+      var myApp.utils.pInt = function(value){
+    		return parseInt(remSpcChr($(value).text()) || 0)
+    	};
+
+      var myApp.utils.nanCheck = function(value) {
+    		if($.isNumeric(value) === false){
+    			return 0
+    		}
+    		else{
+    			return value
+    		}
+    	};
+
+      var myApp.utils.roundOneDec = function(value){
+        return Math.round(value * 10) / 10;
+      };
+
+      var myApp.utils.roundTwoDec = function(value){
+        return Math.round(value * 100) / 100;
+      };
+
+      var myApp.utils.FormatCurrency = function(number) {
+        return (new Intl.NumberFormat('en-EN', {style: 'currency', currency: 'USD' }).format(number))
+      }
+
+      var myApp.utils.FormatPercent = function(number) {
+    		return (number.toFixed(2) + " %")
+    	}
+})()
 
 
-	var remSpcChr = function(value){
-		return value.replace(/[&\/\\#,+()$~%'":*?<>{}]/g, '');
-	}
+// //runs function on a set time delay
+// 	var debouncer = function(func, time) {
+// 		var timeWindow = 500; //time in ms. waits this amount of time after the final click before running function
+// 		var timeout;
+// 		return function() {
+// 			// var context = this;
+// 			clearTimeout(timeout);
+// 			timeout = setTimeout(function() {
+// 				func();
+// 			}, timeWindow)
+// 		} //end function
+// 	}; //end debouncer
 
-	var roundOneDec = function(value){
-		return Math.round(value * 10) / 10;
-	};
 
-	var roundTwoDec = function(value){
-		return Math.round(value * 100) / 100;
-	};
-
-	var pFloat = function(value){
-		return parseFloat(remSpcChr($(value).text()) || 0)
-	}
-
-	var pInt = function(value){
-		return parseInt(remSpcChr($(value).text()) || 0)
-	};
-
-//runs function on a set time delay
-	var debouncer = function(func, time) {
-		var timeWindow = 500; //time in ms. waits this amount of time after the final click before running function
-		var timeout;
-		return function() {
-			// var context = this;
-			clearTimeout(timeout);
-			timeout = setTimeout(function() {
-				func();
-			}, timeWindow)
-		} //end function
-	}; //end debouncer
-
-	var nanCheck = function(value) {
-		if($.isNumeric(value) === false){
-			return 0
-		}
-		else{
-			return value
-		}
-	};
-
-	var FormatCurrency = function(number) {
-		return (new Intl.NumberFormat('en-EN', {style: 'currency', currency: 'USD' }).format(number))
-	}
-
-	var FormatPercent = function(number) {
-		return (number.toFixed(2) + " %")
-	}
 // ============================================================================================================
