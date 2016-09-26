713844931d9f54ed6ab618e403e945ea284691e0
Freddie71010 <freddie71010@gmail.com>
2016-09-26 19:28:36 -0400

    Register process works! WIP: Login system. Issue: user can log in successfully (according to print statements) but the user is redirected back to login page again.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os, re
-from flask import *
 from model import *
+from flask import Flask,session, request, flash, url_for, redirect, render_template, abort, g
+# import flask_login
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
-
-from flask.ext.login import LoginManager
  
 app = Flask(__name__)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -17,75 +18,93 @@ def load_user(id):
 	return User.query.get(int(id))
 
 
+@app.before_request
+def before_request():
+    g.user = current_user
+
 
 # =====================================================================================================
 # Routes ==============================================================================================
 # =====================================================================================================
+
 @app.route("/", methods=['POST','GET'])
 def index():
-	username = ''
+	email = ''
 	if session.get('logged_in'):
-		username = session.get('username')
+		email = session.get('email')
 		return render_template('home.html')
 	return redirect("/main")
 
+@app.route("/main")
+@login_required
+def main():
+	title = "Main Dashboard"
+	return render_template("main.html",
+		title = title)
+
 @app.route("/login", methods=['POST','GET'])
 def login():
-	username = request.form.get('username')
-	password = request.form.get('password')
-	user_obj = User.query.filter_by(username=username).first()
-	print("user_obj:", user_obj)
+	if request.method == 'POST':
+		email = request.form.get('email')
+		password = request.form.get('password')
+		user_obj = User.query.filter_by(email=email).first()
+		print("email:", email)
+		print("user_obj:", user_obj)
 
-	if user_obj:
-		if password == user_obj.password:
-			session['logged_in'] = True
-			session['username'] = user_obj.username
-			return redirect('/main')
+		if user_obj:
+			if password == user_obj.password:
+				session['logged_in'] = True
+				session['email'] = user_obj.email
+				print("successfully logged in")
+				return redirect('/main')
+		error_msg = "Incorrect email/password. Please try again."
 	return render_template("login.html")
+
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+	if request.method == 'GET':
+		return render_template('register.html')
+	user = User(request.form['email'], request.form['password'], request.form['firstname'], request.form['lastname'])
+	db.session.add(user)
+	db.session.commit()
+	print("User successfully added.")
+	return redirect('/login')
 
 @app.route("/logout")
 def logout():
-	if session.get('logged_in') == True:
-		session['logged_in'] = False
-		session['username'] = False
+	logout_user()
 	return redirect('/login')
 
 
-@app.route("/register")
-def register():
-	pass
-
 @app.route("/dashboard", methods=['GET','POST'])
+@login_required
 def dashboard():
 	title = "Dashboard"
 	return render_template("dashboard.html",
 		title = title)
 
-@app.route("/performa")
+@app.route("/performa", methods=['GET','POST'])
+@login_required
 def performa():
 	title = "Performa"
 	return render_template("performa.html",
 		title=title)
 
-@app.route('/returns_summary')
+@app.route('/returns_summary', methods=['GET','POST'])
+@login_required
 def returnSum():
 	title = "Returns Summary"
 	return render_template('returns_summary.html',
 		title=title)
 
 @app.route('/inputform')
+@login_required
 def inputForm():
 	title="Input Form"
 	return render_template('input_form.html',
 		title = title)
 
-@app.route("/main")
-def main():
-	login_redirect()
-	print("===We are here")
-	title = "Main Dashboard"
-	return render_template("main.html",
-		title = title)
 
 
 
