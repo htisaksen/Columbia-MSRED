3543ed47b5354229a4441b29f6300958a8a59259
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2016-10-06 16:56:04 -0400

    small visual changes

--- static/js/behavior.js
+++ static/js/behavior.js
@@ -14,5 +14,10 @@ $(document).ready(function(){
   });
 
   $('.modal-trigger').leanModal();
+  
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 
 });
