577af51a5c101447e81eb18c69804f9a593e6cb3
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2016-10-06 18:53:27 -0400

    changed backround to fade grey/greyblue and changed the headers to do the same

--- static/js/behavior.js
+++ static/js/behavior.js
@@ -13,8 +13,10 @@ $(document).ready(function(){
     accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
   });
 
-  $('.modal-trigger').leanModal();
-  
+  $('.modal-trigger').leanModal({
+    opacity:.44,
+  });
+
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
