0e5a70959f13e373d763f4d2757c7d8897a41be7
Freddie71010 <freddie71010@gmail.com>
2016-10-12 23:33:36 -0400

    Unlevered Analysis table fixed, auto column generation 100%. formatted row color and borders.

--- static/js/returns_summary.js
+++ static/js/returns_summary.js
@@ -14,25 +14,30 @@ myApp.returnsSummary = function(){
 		if (g.saleYear < rsCounter+1) {
 			$('#unlevered-analysis thead tr th:not(:first)').each(function(){
 				$(this).remove();
+				console.log("Removed thead row");
 			});
 			$('#unlevered-analysis tbody tr td').each(function(){
 				$(this).remove();
+				console.log("Removed tbody row");
 			})
 		};
 
 		//Inserts number of columns based on Sale Year in Dashboard
-		for(var i = 0; i < g.saleYear+1; ++i) {
-			
+		for(var i = 1; i < g.saleYear+2; ++i) {
 			rsCounter = $('#unlevered-analysis .RS_Year_End th').length;
-			console.log("rsCounter:",rsCounter);
-			var Rental_Rate_Income_Total = pFloat('#Rental_Income_Total');
-	
-			$('#unlevered-analysis thead tr:first').append("<th class= 'RS_" + "year_" + rsCounter + "'>"+'YEAR '+ rsCounter +"</th>");
-			if (i == 0){
-			//Year 1 data grabbed from org_dashboard calculations
+			
+			if (i == 1){
+				//Cost value grabbed
+				$('#unlevered-analysis thead tr:first').append("<th>COSTS</th>");		
+				$('#unlevered-analysis tbody tr:not(:last)').each(function(){
+					$(this).append("<td></td>");
+				})
+				$('#unlevered-analysis tbody tr:last').append("<td>Value</td>");
 
 			} else {
 			//All additional year data calculated based on Pro Forma table calculations ===================
+				$('#unlevered-analysis thead tr:first').append("<th class= 'RS_" + "year_" + (rsCounter-1) + "'>"+'YEAR '+ (rsCounter-1) +"</th>");
+				
 				//RS_Net_Operating_Income
 				$('#unlevered-analysis tbody tr:nth-child(1)').append(
 					"<td class= 'RS_Net_Operating_Income'>" +
@@ -53,6 +58,8 @@ myApp.returnsSummary = function(){
 					 pFloat('.RS_Other_Income td:nth-child('+ (i+1) +')')
 					) + "</td>";
 				
+				$('#unlevered-analysis tbody tr:nth-child(4)').append("<td></td>");
+
 				//RS_Gross_Sale_Proceeds
 				$('#unlevered-analysis tbody tr:nth-child(5)').append(
 					"<td class= 'RS_Gross_Sale_Proceeds'>(" +
@@ -72,9 +79,10 @@ myApp.returnsSummary = function(){
 					(1+($('#year_row_' + i + ' .mkt_rent_credit_loss').val())/100)) +
 					")</td>";
 				
-				//RS_Net_Cash_Flow_from_Operations
+				$('#unlevered-analysis tbody tr:nth-child(8)').append("<td></td>");
+				//RS_Net_Cash_Flow_from_Operations2
 				$('#unlevered-analysis tbody tr:nth-child(9)').append(
-					"<td class= 'RS_Net_Cash_Flow_from_Operations'>" +
+					"<td class= 'RS_Net_Cash_Flow_from_Operations2'>" +
 					pFloat('#unlevered-analysis .RS_Real_Estate_Taxes td:nth-child('+ (i+1) +')')  *
 					(1+($('#year_row_' + i + ' .mkt_rent_expenses').val())/100)) +
 					"</td>";
@@ -85,6 +93,7 @@ myApp.returnsSummary = function(){
 					(1+($('#year_row_' + i + ' .mkt_rent_expenses').val())/100)) +
 					"</td>";
 
+				$('#unlevered-analysis tbody tr:nth-child(11)').append("<td></td>");
 				//RS_Total_Cash_Flows
 				$('#unlevered-analysis tbody tr:nth-child(12)').append(
 					"<td class= 'RS_Total_Cash_Flows'>" +
