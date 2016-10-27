023489ba61c8b05e249a2e285abc60d06fe23e31
Freddie71010 <freddie71010@gmail.com>
2016-10-27 14:40:59 -0400

    Levered Tbl is currently set as  a direct duplicate of Unlevered Tbl.  WIP: Update return summary JS for Levered table to incorporate the additional 'RS_Less_Debt_Service' calculated row. this means we need to set the child # to a variable instead of hard code it

--- static/js/returns_summary.js
+++ static/js/returns_summary.js
@@ -9,119 +9,148 @@ myApp.returnsSummary = function(){
 		var pFloat = myApp.utils.pFloat;
 
 		var rsCounter = g.saleYear;
+		var intRate = g.interestRateOnMortgage;
+	
 
+	var RSgenerator = function(tbl_name){
 	//Deletes all columns
 		if (g.saleYear < rsCounter+1) {
-			$('#unlevered-analysis thead tr th:not(:first)').each(function(){
+			$(tbl_name +'thead tr th:not(:first)').each(function(){
 				$(this).remove();
 			});
-			$('#unlevered-analysis tbody tr td').each(function(){
+			$(tbl_name +'tbody tr td').each(function(){
 				$(this).remove();
 			})
 		};
 
 		//Inserts number of columns based on Sale Year in Dashboard
 		for(var i = 1; i < g.saleYear+2; ++i) {
-			rsCounter = $('#unlevered-analysis .RS_Year_End th').length;
+			rsCounter = $(tbl_name +'.RS_Year_End th').length;
 
 			if (i == 1){
 				//Cost value grabbed
-				$('#unlevered-analysis thead tr:first').append("<th>COSTS</th>");
-				$('#unlevered-analysis tbody tr:not(:last)').each(function(){
+				$(tbl_name +'thead tr:first').append("<th>COSTS</th>");
+				$(tbl_name +'tbody tr:not(:last)').each(function(){
 					$(this).append("<td></td>");
 				})
-				$('#unlevered-analysis tbody tr:last').append("<td>" +
+				$(tbl_name +'tbody tr:last').append("<td>" +
 					FormatCurrency(pFloat('#dashboard #Purchase_Information td#PI_Total_Costs')*-1) +
 					"</td>");
 
 			} else {
 			//All additional year data calculated based on Pro Forma table calculations ===================
-				$('#unlevered-analysis thead tr:first').append("<th class= 'RS_" + "year_" + (rsCounter-1) + "'>"+'YEAR '+ (rsCounter-1) +"</th>");
+				$(tbl_name +'thead tr:first').append("<th class= 'RS_" + "year_" + (rsCounter-1) + "'>"+'YEAR '+ (rsCounter-1) +"</th>");
 
 				//RS_Net_Operating_Income
-				$('#unlevered-analysis tbody tr:nth-child(1)').append(
+				$(tbl_name +'tbody tr:nth-child(1)').append(
 					"<td class= 'RS_Net_Operating_Income'>" +
 					FormatCurrency(
 					 pFloat('#Proforma tbody td.PF_Net_Operating_Income:nth-child('+ i +')')) +
 					"</td>");
 				//RS_Less_Capital_Expenditures
-				$('#unlevered-analysis tbody tr:nth-child(2)').append(
+				$(tbl_name +'tbody tr:nth-child(2)').append(
 					"<td class= 'RS_Less_Capital_Expenditures'>" +
 					FormatCurrency(
 					 pFloat('#Proforma tbody td.PF_Capital_Expenditures:nth-child('+ i +')')*-1) +
 					"</td>");
-				//RS_Net_Cash_Flow_from_Operations
-				$('#unlevered-analysis tbody tr:nth-child(3)').append(
-					"<td class= 'RS_Net_Cash_Flow_from_Operations'>" +
-					FormatCurrency(
-					 pFloat('#unlevered-analysis td.RS_Net_Operating_Income:last') +
-					 pFloat('#unlevered-analysis td.RS_Less_Capital_Expenditures:last')
-					) + "</td>");				
 				
+				//RS_Less_Debt_Service - LEVERED ONLY
+				// if (tbl_name = "#levered-analysis ") {
+				
+				// WIP NOTES =======================================================================================
+					// In order for the Levered table to work correctly, you first need to include a "RS_Less_Debt_Service" row.
+					// The issue is that since the child numbers are hard coded, and we are currently just looping through the main RSgenerator function again with a diff table name,
+					// the insertion of a new row will mess up all subsequent generated rows.  TO DO: Replace hard coded values with variable and increment through. 
+				
+				// $(tbl_name +'tbody tr:nth-child(2)').append(
+				// 	"<td class= 'RS_Less_Debt_Service'>-$100,000</td>");
+
+				//RS_Net_Cash_Flow_from_Operations - Levered
+					// $(tbl_name +'tbody tr:nth-child(3)').append(
+					// 	"<td class= 'RS_Net_Cash_Flow_from_Operations'>" +
+					// 	FormatCurrency(
+					// 	 pFloat(tbl_name +'td.RS_Net_Operating_Income:last') +
+					// 	 pFloat(tbl_name +'td.RS_Less_Capital_Expenditures:last') +
+					// 	 pFloat(tbl_name +'td.RS_Less_Debt_Service:last')
+					// 	) + "</td>");
+
+					//RS_Net_Cash_Flow_from_Operations - Unlevered
+					$(tbl_name +'tbody tr:nth-child(3)').append(
+						"<td class= 'RS_Net_Cash_Flow_from_Operations'>" +
+						FormatCurrency(
+						 pFloat(tbl_name +'td.RS_Net_Operating_Income:last') +
+						 pFloat(tbl_name +'td.RS_Less_Capital_Expenditures:last')
+						) + "</td>");
+
 				//Filler line
-				$('#unlevered-analysis tbody tr:nth-child(4)').append("<td></td>");
+				$(tbl_name +'tbody tr:nth-child(4)').append("<td></td>");
 
 				//RS_Gross_Sale_Proceeds
 				if (i == g.saleYear+1) {
-					$('#unlevered-analysis tbody tr:nth-child(5)').append(
+					$(tbl_name +'tbody tr:nth-child(5)').append(
 					"<td class= 'RS_Gross_Sale_Proceeds'>" +
 					FormatCurrency(
 					 pFloat('#Proforma tbody td.PF_Net_Operating_Income:last') / (g.terminalCapRate/100)
 					) + "</td>");
 				} else {
-					$('#unlevered-analysis tbody tr:nth-child(5)').append(
+					$(tbl_name +'tbody tr:nth-child(5)').append(
 					"<td class= 'RS_Gross_Sale_Proceeds'>$0.00</td>");
 				}
 				//RS_Sales_Costs
-				if (pFloat('#unlevered-analysis tbody td.RS_Gross_Sale_Proceeds:last') > 0) {
-					$('#unlevered-analysis tbody tr:nth-child(6)').append(
+				if (pFloat(tbl_name +'tbody td.RS_Gross_Sale_Proceeds:last') > 0) {
+					$(tbl_name +'tbody tr:nth-child(6)').append(
 					"<td class= 'RS_Sales_Costs'>" +
 					FormatCurrency(
-					 pFloat('#unlevered-analysis tbody td.RS_Gross_Sale_Proceeds:last') * (g.salesCosts/100) * -1
+					 pFloat(tbl_name +'tbody td.RS_Gross_Sale_Proceeds:last') * (g.salesCosts/100) * -1
 					) + "</td>");
 				} else {
-					$('#unlevered-analysis tbody tr:nth-child(6)').append(
+					$(tbl_name +'tbody tr:nth-child(6)').append(
 					"<td class= 'RS_Sales_Costs'>$0.00</td>");
 				}
 				//RS_Net_Sales_Proceeds
-				$('#unlevered-analysis tbody tr:nth-child(7)').append(
+				$(tbl_name +'tbody tr:nth-child(7)').append(
 					"<td class= 'RS_Net_Sales_Proceeds'>" +
 					FormatCurrency(
-					 pFloat('#unlevered-analysis td.RS_Gross_Sale_Proceeds:last')  +
-					 pFloat('#unlevered-analysis td.RS_Sales_Costs:last')
+					 pFloat(tbl_name +'td.RS_Gross_Sale_Proceeds:last')  +
+					 pFloat(tbl_name +'td.RS_Sales_Costs:last')
 					) + "</td>");
 
 				//filler line
-				$('#unlevered-analysis tbody tr:nth-child(8)').append("<td></td>");
+				$(tbl_name +'tbody tr:nth-child(8)').append("<td></td>");
 				
 				//RS_Net_Cash_Flow_from_Operations2
-				$('#unlevered-analysis tbody tr:nth-child(9)').append(
+				$(tbl_name +'tbody tr:nth-child(9)').append(
 					"<td class= 'RS_Net_Cash_Flow_from_Operations2'>" +
 					FormatCurrency(
-					 pFloat('#unlevered-analysis td.RS_Net_Operating_Income:last') +
-					 pFloat('#unlevered-analysis td.RS_Less_Capital_Expenditures:last')
+					 pFloat(tbl_name +'td.RS_Net_Operating_Income:last') +
+					 pFloat(tbl_name +'td.RS_Less_Capital_Expenditures:last')
 					) + "</td>");	
 				//RS_Net_Sales_Proceeds2
-				$('#unlevered-analysis tbody tr:nth-child(10)').append(
+				$(tbl_name +'tbody tr:nth-child(10)').append(
 					"<td class= 'RS_Net_Sales_Proceeds2'>" +
 					FormatCurrency(
-					 pFloat('#unlevered-analysis td.RS_Gross_Sale_Proceeds:last')  +
-					 pFloat('#unlevered-analysis td.RS_Sales_Costs:last')
+					 pFloat(tbl_name +'td.RS_Gross_Sale_Proceeds:last')  +
+					 pFloat(tbl_name +'td.RS_Sales_Costs:last')
 					) + "</td>");
 
 				//filler line
-				$('#unlevered-analysis tbody tr:nth-child(11)').append("<td></td>");
+				$(tbl_name +'tbody tr:nth-child(11)').append("<td></td>");
 				
 				//RS_Total_Cash_Flows
-				$('#unlevered-analysis tbody tr:nth-child(12)').append(
+				$(tbl_name +'tbody tr:nth-child(12)').append(
 					"<td class= 'RS_Total_Cash_Flows'>" +
 					FormatCurrency(
-					 pFloat('#unlevered-analysis .RS_Net_Sales_Proceeds2 td:nth-child('+ (i+1) +')')  +
-					 pFloat('#unlevered-analysis .RS_Net_Cash_Flow_from_Operations2 td:nth-child('+ (i+1) +')')
+					 pFloat(tbl_name +'.RS_Net_Sales_Proceeds2 td:nth-child('+ (i+1) +')')  +
+					 pFloat(tbl_name +'.RS_Net_Cash_Flow_from_Operations2 td:nth-child('+ (i+1) +')')
 					) + "</td>");
 
 
 			} //end else
 		}; //end for loop
+	}; //end RSgenerator
+
+	RSgenerator("#unlevered-analysis ");
+	RSgenerator("#levered-analysis ");
+
 
 } //end myApp.returnSummary function
